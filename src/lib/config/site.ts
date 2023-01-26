73e6fd38a8b24a889b52049842fa43aac5995d35
munHunger <marcusmunger@gmail.com>
2023-01-26 20:58:46 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -4,14 +4,14 @@ export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'blog.wunderdev.com',
   title: 'Wunderdev',
-  subtitle: 'A personal blog about my learning and my hobby projects',
+  subtitle: 'A personal blog about my hobby projects',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
     avatar: '/assets/profile.jpg',
     name: 'Marcus Münger',
     status: '🌸',
-    bio: 'Fullstack developer with a passion for learning'
+    bio: 'Exploring the world one commit at a time'
   },
   themeColor: '#3D4451'
 }
